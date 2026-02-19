dd4fba8573e01aa4f8e4c0f903b63b3974eae130
Lishitha Kudala <kudalalishitha@gmail.com>
2026-02-19 20:59:46 +0530

    Add Product entity model

---
+++ product-service/src/main/java/com/example/product_service/model/Product.java
@@ -0,0 +1,25 @@
+package com.example.product_service.model;
+
+import jakarta.persistence.*;
+        import lombok.*;
+
+@Entity
+@Table(name = "products")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private double price;
+
+    private int quantity;
+}
